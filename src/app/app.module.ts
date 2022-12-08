de7ea2d5ceb4d89a78891bdbf270e8269981a49b
Isaac Brian <ISANT104@ford.com>
2022-12-08 16:34:27 -0300

    initial: first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule } from '@angular/material/menu'
+import {MatButtonModule } from '@angular/material/button'
+import {MatIconModule } from '@angular/material/icon'
+import {MatSidenavModule } from '@angular/material/sidenav'
+import {MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './modules/components/home/home.component';
+import { GamesComponent } from './modules/components/games/games.component';
+import { FavoritesComponent } from './modules/components/favorites/favorites.component';
+import { MoviesComponent } from './modules/components/movies/movies.component';
+import { SeriesComponent } from './modules/components/series/series.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GamesComponent,
+    FavoritesComponent,
+    MoviesComponent,
+    SeriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
