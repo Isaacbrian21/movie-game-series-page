65c876d628e4d8b6c365119bf1febe472810d286
Isaac Brian <ISANT104@ford.com>
2022-12-10 13:00:27 -0300

    implementing: initialized and working lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {path: 'home', loadChildren: () => import('../app/modules/components/homes/homes.module').then(home => home.HomesModule)},
+
+  {path:'games', loadChildren: () => import('../app/modules/components/game/game.module').then(games => games.GameModule)},
+
+  {path:'favorite', loadChildren: () => import('../app/modules/components/fav/fav.module').then(fav => fav.FavModule)},
+
+  {path:'movies', loadChildren: () => import('../app/modules/components/movie/movie.module').then(movies => movies.MovieModule)},
+
+  {path:'series', loadChildren: () => import('../app/modules/components/serie/serie.module').then(series => series.SerieModule)}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
